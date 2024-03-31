f2bfd09e9976e1abf4b52c5316c1b2414fce6387
justastranger <justastrangerlica@gmail.com>
2024-03-30 20:23:07 -0500

    Add project files.

---
+++ ArtisanProductsCopyQuality/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtisanProductsCopyQuality
+{
+    internal class Config
+    {
+        internal readonly HashSet<string> machinesToTarget;// = new() { "(BC)17", "(BC)12", "(BC)15", "(BC)16", "(BC)24", "(BC)19", "(BC)Dehydrator", "(BC)FishSmoker" };
+
+        public Config()
+        {
+            machinesToTarget = new() { "(BC)17", "(BC)12", "(BC)15", "(BC)16", "(BC)24", "(BC)19", "(BC)Dehydrator", "(BC)FishSmoker" };
+        }
+    }
+}
---
+++ ArtisanProductsCopyQuality/ModEntry.cs
@@ -0,0 +1,41 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley.GameData.Machines;
+
+namespace ArtisanProductsCopyQuality
+{
+    public class ModEntry : Mod
+    {
+        internal static ModEntry instance;
+        internal Config config;
+
+        public override void Entry(IModHelper helper)
+        {
+            instance = this;
+            config = helper.ReadConfig<Config>();
+            Helper.Events.Content.AssetRequested += AssetRequested;
+        }
+
+        private void AssetRequested(object? sender, AssetRequestedEventArgs ev)
+        {
+            if (ev.NameWithoutLocale.IsEquivalentTo("Data/Machines"))
+                ev.Edit(EditMachines, AssetEditPriority.Default);
+        }
+
+        private void EditMachines(IAssetData asset)
+        {
+            if (asset.Data is Dictionary<string, MachineData> data)
+            {
+                foreach (KeyValuePair<string, MachineData> machine in data)
+                {
+                    if (!config.machinesToTarget.Contains(machine.Key)) continue;
+                    machine.Value?.OutputRules.ForEach(rule => {
+                        rule?.OutputItem.ForEach(item => {
+                            if (item is not null) item.CopyQuality = true;
+                        });
+                    });
+                }
+            }
+        }
+    }
+}
